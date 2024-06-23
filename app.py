dfdf0ae39692eb7826bf6b27a0ca9313e7242536
Junaidkhan0341 <junaidkh0092@gmail.com>
2024-06-24 02:11:31 +0500

    Prepare for deployment api new one

--- app.py
+++ app.py
@@ -26,5 +26,4 @@ def remove_background():
         return jsonify({"error": f"Background removal failed: {e}"}), 500
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=10000)
